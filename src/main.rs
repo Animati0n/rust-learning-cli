ed3f83622f914f8f10abdf4d38730b2e908aac8c
Abhishek <kvabhishek1227@gmail.com>
2025-04-15 12:22:45 +0530

    Added --help and argument help
    Added --help option and description arguments for user who may don't
    know how to use the cli.
    
    Features to add
    ----------------
     - using std::fs::read_to_string(&args.path) which is not best
       implematation but working model because "it reads the whole file into
       memory regardless of size of file.
    
     - instead of std::fs::read_to_string(&args.path)use  std::io::BufReader
       which may optimaize memory useage when large files are given.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
+use clap::Parser;
 
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
     path: std::path::PathBuf,
 }
 
-
 fn main() {
 
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-
-    let args=Cli{
-        pattern,
-        path:std::path::PathBuf::from(path),
-    };
-
-    println!("pattern: {:?}, path: {:?}", args.pattern,args.path);
+    let args = Cli::parse();
+    
+    let content= std::fs::read_to_string(&args.path).expect("Could not read fle");
+    
+    for line in content.lines(){
+        if line.contains(&args.pattern){
+            println!("{}",line);
+        }
+    }
 }
 
