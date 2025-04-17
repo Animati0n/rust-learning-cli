c202581ef35d03c2940343d2515a5f8f9b38ba0e
Abhishek <kvabhishek1227@gmail.com>
2025-04-17 19:28:42 +0530

    Added custom error handler
    Created custom error handle for printing custom error.
    
    Faced issue for map_err() format!()
    
    - path type datas should use display() method implemented in the data
      type
    - some times we manually has to implement expecially when
    data type is structure

--- src/main.rs
+++ src/main.rs
@@ -10,112 +10,53 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+/// Custom  error handleing
+
+struct CustomError(String);
+
+/// chagpt code  
+
+/// for handling erron differnet type.
+
+/// `PathBuf` cannot be formatted with the default formatter; call `.display()` on it
+/// .map_err(|err| CustomError(format!("Error reading `{}`: {}",&args.path,err)))?;
+
+impl std::fmt::Display for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+/// `CustomError` doesn't implement `Debug`
+/// `CustomError` cannot be formatted using `{:?}`
+/// fn main()->Result<(),CustomError> {
+
+impl std::fmt::Debug for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.0)
+    }
+
+}
+
+/// expected `Result<(), CustomError>`, found `()`
+/// fn main()->Result<(),CustomError> {
+
+impl std::error::Error for CustomError {}
+
+
+
+fn main()->Result<(),CustomError> {
 
     let args = Cli::parse();
-    
-    let content= std::fs::read_to_string(&args.path).expect("Could not read fle");
-    
+
+    let content= std::fs::read_to_string(&args.path)
+        .map_err(|err| CustomError(format!("Error reading `{}`: {}",&args.path.display(),err)))?;
+
     for line in content.lines(){
         if line.contains(&args.pattern){
             println!("{}",line);
         }
     }
+    Ok(())
 }
 
-/// Error handling methods other than  except() if file not found
-///
-/// Method 1
-/// fn main() {
-
-/// let args = Cli::parse();
-/// let result = std::fs::read_to_string(&args.path);
-/// match result {
-///    Ok(content) => { println!("File content: {}", content); }
-///        Err(error) => { println!("Oh noes: {}", error); }
-///        }
-
-///    for line in content.lines(){
-///        if line.contains(&args.pattern){
-///            println!("{}",line);
-///        }
-///    }
-///}
-
-/// The disadvantage of this method is that "content" is accessible beyond "match case".
-/// -------------------------------------------------------------------------------------
-
-/// Method 2
-///
-///
-/// This method allows unwrapping founctionallity in  "match" using "{ value }"
-/// and "panic" to exit(abort progarme) if error. 
-///
-///fn main() {
-
-///    let args = Cli::parse();
-/// let result = std::fs::read_to_string(&args.path);
-/// let content = match result {
-///     Ok(content) => { content },
-///     Err(error) => { panic!("Can't deal with {}, just exit here", error); }
-/// };
-/// or
-/// Short cut for above method is 
-/// // let content = std::fs::read_to_string(&args.path).unwrap();
-///
-/// println!("file content: {}", content);
-///    for line in content.lines(){
-///        if line.contains(&args.pattern){
-///            println!("{}",line);
-///        }
-///    }
-///}
-///
-///--------------------------------------------------------------------------------------
-
-
-/// Method 3
-///
-///
-/// Dealing error  without "panic!()"  and return
-///
-///
-/// fn main() -> Result<(), Box<dyn std::error::Error>> {
-/// let args = Cli::parse();
-///     let result = std::fs::read_to_string(&args.path);
-///     let content = match result {
-///           Ok(content) => { content },
-///           Err(error) => { return Err(error.into()); }
-///     };
-///     println!("file content: {}", content);
-///    for line in content.lines(){
-///        if line.contains(&args.pattern){
-///            println!("{}",line);
-///        }
-///    }
-///     Ok(())
-///   }
-///   ===================================================================================
-
-
-
-/// Method 4
-///
-/// You can append "?" operator to a value of type "Result", and Rust will internally expand
-/// this to something very similar to the match we just wrote.
-///
-/// fn main() -> Result<(), Box<dyn std::error::Error>> {
-/// let args = Cli::parse();
-///     let result = std::fs::read_to_string(&args.path);
-///     let content = std::fs::read_to_string(&args.path)?;
-///         println!("file content: {}", content);
-///    for line in content.lines(){
-///        if line.contains(&args.pattern){
-///            println!("{}",line);
-///        }
-///    }
-///             Ok(())
-///             }
-///
-///--------------------------------------------------------------------------------------
-///
