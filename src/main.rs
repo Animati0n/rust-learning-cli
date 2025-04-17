e1bd0f09a2fb9f5baf21ad1460098fb7a2868305
Abhishek <kvabhishek1227@gmail.com>
2025-04-17 19:53:01 +0530

    Added anyhow dependencie for better error messages
    Creating custom error is goood one but jsut showing error
    as custom cause to lose actual error to be ignored. So any how package
    with it's context tarit helps to append custom error and also get actual
    error chained with it.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use anyhow::{Context, Result};
 
 /// Search for a pattern in a file and display the lines that contain it.
 
@@ -10,48 +11,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-/// Custom  error handleing
 
-struct CustomError(String);
-
-/// chagpt code  
-
-/// for handling erron differnet type.
-
-/// `PathBuf` cannot be formatted with the default formatter; call `.display()` on it
-/// .map_err(|err| CustomError(format!("Error reading `{}`: {}",&args.path,err)))?;
-
-impl std::fmt::Display for CustomError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-/// `CustomError` doesn't implement `Debug`
-/// `CustomError` cannot be formatted using `{:?}`
-/// fn main()->Result<(),CustomError> {
-
-impl std::fmt::Debug for CustomError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.0)
-    }
-
-}
-
-/// expected `Result<(), CustomError>`, found `()`
-/// fn main()->Result<(),CustomError> {
-
-impl std::error::Error for CustomError {}
-
-
-
-fn main()->Result<(),CustomError> {
+fn main()->Result<()> {
 
     let args = Cli::parse();
 
     let content= std::fs::read_to_string(&args.path)
-        .map_err(|err| CustomError(format!("Error reading `{}`: {}",&args.path.display(),err)))?;
-
+        .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
     for line in content.lines(){
         if line.contains(&args.pattern){
             println!("{}",line);
